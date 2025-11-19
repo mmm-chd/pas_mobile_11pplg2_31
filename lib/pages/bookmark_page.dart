b4c10e0ccf77bb8e46e4dbeab9c90e7608f7c94f
unknown <melvinmulyono02@gmail.com>
2025-11-19 11:56:30 +0700

    second commit

--- lib/pages/bookmark_page.dart
+++ lib/pages/bookmark_page.dart
@@ -18,119 +18,110 @@ class BookmarkPage extends StatelessWidget {
       body: Center(
         child: Obx(() {
           final List<TvShowModel> bookmarks = showController.showsList
-              .where((p) => p.isBookmarked)
+              .where((show) => show.isBookmarked)
               .toList();
+
           if (showController.isLoading.value) {
             return const Center(child: CircularProgressIndicator());
           }
 
-          if (bookmarkController.bookmarkedShows.isEmpty) {
+          if (bookmarks.isEmpty) {
             return const Center(child: MytextWidget(text: 'Bookmark is Empty'));
           }
 
-          return RefreshIndicator(
-            onRefresh: () {
-              return showController.fetchTvShows();
-            },
-            child: ListView.builder(
-              itemCount: bookmarks.length,
-              itemBuilder: (context, index) {
-                final tvShow = bookmarks[index];
+          return ListView.builder(
+            itemCount: bookmarks.length,
+            itemBuilder: (context, index) {
+              final tvShow = bookmarks[index];
 
-                return Card(
-                  color: Colors.white,
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 5,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                            tvShow.image?.medium ?? '',
-                            height: 150,
-                            width: 100,
-                            fit: BoxFit.cover,
-                          ),
+              return Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                          tvShow.image.medium,
+                          height: 150,
+                          width: 100,
+                          fit: BoxFit.cover,
                         ),
-                        const MyspaceWidget(width: 10),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  SizedBox(
-                                    width: 200,
-                                    child: MytextWidget(
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16,
-                                      ),
-                                      maxLines: 1,
-                                      text: '${tvShow.name}',
+                      ),
+                      const MyspaceWidget(width: 10),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                SizedBox(
+                                  width: 200,
+                                  child: MytextWidget(
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
                                     ),
+                                    maxLines: 1,
+                                    text: '${tvShow.name}',
                                   ),
-                                  GestureDetector(
-                                    onTap: () {
-                                      bookmarkController.toogleBookmark(
-                                        tvShow.id,
-                                      );
-                                    },
-                                    child: Icon(
-                                      tvShow.isBookmarked
-                                          ? Icons.favorite
-                                          : Icons.favorite_border,
-                                      color: tvShow.isBookmarked
-                                          ? Colors.red
-                                          : Colors.grey,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const MyspaceWidget(height: 8),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    children: [
-                                      const Icon(
-                                        Icons.star,
-                                        size: 16,
-                                        color: Colors.amber,
-                                      ),
-                                      const MyspaceWidget(width: 4),
-                                      MytextWidget(
-                                        text: '${tvShow.rating.average}',
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                              const MyspaceWidget(height: 8),
-                              MytextWidget(
-                                maxLines: 4,
-                                text: tvShow.summary.replaceAll(
-                                  RegExp(r'<[^>]*>'),
-                                  '',
                                 ),
+                                GestureDetector(
+                                  onTap: () {
+                                    bookmarkController.toogleBookmark(
+                                      tvShow.id,
+                                    );
+                                  },
+                                  child: Icon(
+                                    tvShow.isBookmarked
+                                        ? Icons.favorite
+                                        : Icons.favorite_border,
+                                    color: tvShow.isBookmarked
+                                        ? Colors.red
+                                        : Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const MyspaceWidget(height: 8),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    const Icon(
+                                      Icons.star,
+                                      size: 16,
+                                      color: Colors.amber,
+                                    ),
+                                    const MyspaceWidget(width: 4),
+                                    MytextWidget(
+                                      text: '${tvShow.rating.average}',
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            const MyspaceWidget(height: 8),
+                            MytextWidget(
+                              maxLines: 4,
+                              text: tvShow.summary.replaceAll(
+                                RegExp(r'<[^>]*>'),
+                                '',
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                );
-              },
-            ),
+                ),
+              );
+            },
           );
         }),
       ),
